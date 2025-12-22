69014e076e7e5e6ba3e128953de8fdc020210c64
gowthamflm <vengalareddy.challa@gmail.com>
2025-12-22 10:05:41 +0530

    all2

--- day-11/githubint.py
+++ day-11/githubint.py
@@ -3,3 +3,7 @@ import requests
 url = f'https://api.github.com/repos/iam-veeramalla/python-for-devops/pulls'
 
 response = requests.get(url)
+
+if response.status_code == 200:
+    # Convert the JSON response to a dictionary
+    pull_requests = response.json()
